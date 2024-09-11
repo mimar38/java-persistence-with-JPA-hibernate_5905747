a18d7f4c4b41b3184a6dce3a2ad23de09fab7940
smoser-LiL <smoser-LiL@localhost>
2024-09-11 21:29:33 +0000

    Moving files  using main

---
+++ src/main/java/com/mycompany/app/entities/Book.java
@@ -0,0 +1,37 @@
+package com.mycompany.app.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="book")
+public class Book {
+
+  @Column(name="book_id")
+  private int id;
+  @Column(name = "book_name")
+  private String name;
+  private String isbn;
+  public int getId() {
+    return id;
+  }
+  public void setId(int id) {
+    this.id = id;
+  }
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public String getIsbn() {
+    return isbn;
+  }
+  public void setIsbn(String isbn) {
+    this.isbn = isbn;
+  }
+
+  
+
+}
